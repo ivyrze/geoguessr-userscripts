f51d68185bfbe65add0924d6e42c0dfa0508da9f
Ivy Rose <hi@ivyrose.me>
2024-02-15 08:23:22 -0500

    Created bullseye keybinding script

--- README.md
+++ README.md
@@ -5,3 +5,4 @@
 First, you'll need to install a userscript browser extension like [Tampermonkey](https://www.tampermonkey.net). Then, click the link corrosponding to the script you'd like to download:
 
 - [Fast Move Keybinding](https://github.com/ivyrze/geoguessr-userscripts/raw/main/fastmove-keybind.user.js)
+- [Bullseye Keybinding](https://github.com/ivyrze/geoguessr-userscripts/raw/main/bullseye-keybind.user.js)
---
+++ bullseye-keybind.user.js
@@ -0,0 +1,81 @@
+// ==UserScript==
+// @name         GeoGuessr Bullseye Keybinding
+// @description  Customizable keybinding for adjusting the bullseye circle size
+// @version      1.0
+// @author       notsopoisonous
+// @match        https://www.geoguessr.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=geoguessr.com
+// @require      https://openuserjs.org/src/libs/sizzle/GM_config.min.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
+// @downloadURL  https://github.com/ivyrze/geoguessr-userscripts/raw/main/fastmove-keybind.user.js
+// @updateURL    https://github.com/ivyrze/geoguessr-userscripts/raw/main/fastmove-keybind.user.js
+// ==/UserScript==
+
+(async function() {
+    'use strict';
+
+    let configFields = {
+        increaseKey: {
+            label: 'Increase Size Key',
+            type: 'text',
+            default: '['
+        },
+        decreaseKey: {
+            label: 'Decrease Size Key',
+            type: 'text',
+            default: ']'
+        }
+    };
+    let configValues = {};
+
+    const updateConfigValues = function () {
+        Object.keys(configFields).forEach(id => {
+            configValues[id] = this.get(id);
+        });
+    };
+
+    /* globals GM_config */
+    GM_config.init({
+        id: 'bullseye-keybind',
+        title: 'Bullseye Settings',
+        fields: configFields,
+        events: {
+            save: updateConfigValues,
+            init: updateConfigValues
+        }
+    });
+    GM_registerMenuCommand("Configure", () => GM_config.open());
+
+    const isRelevantKey = (key) => {
+        return (key == configValues.increaseKey || key == configValues.decreaseKey);
+    };
+
+    document.addEventListener('keydown', (event) => {
+        if (!isRelevantKey(event.key)) { return; }
+
+        const container = document.querySelector("[class^=\"radius-controls_wrapper\"]");
+        
+        if (!container) { return false; }
+        
+        // Get index of currently selected circle size
+        let selectedIndex = 5 - parseInt(container.querySelector("[class^=\"styles_rangeslider\"]").ariaValueNow);
+
+        const increaseMode = event.key == configValues.increaseKey;
+        if (increaseMode && selectedIndex < 5) {
+            selectedIndex++;
+        } else if (!increaseMode && selectedIndex > 0) {
+            selectedIndex--;
+        }
+
+        // Find and get positioning of the to-be selected label
+        const target = container.querySelector("[class^=\"styles_labels\"]").children[selectedIndex];
+        const { x: clientX, y: clientY } = target.getBoundingClientRect();
+
+        const remappedEvent = new MouseEvent('mousedown', { clientX, clientY, bubbles: true });
+        target.dispatchEvent(remappedEvent);
+
+        return false;
+    });
+})();
